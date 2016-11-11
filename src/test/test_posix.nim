3d2eaeccc6ab83b71ef5261820075ae23ea5e362
johnscillieri <john.scillieri@visionistinc.com>
2016-11-10 20:10:24 -0500

    add initial test_posix code

---
+++ src/test/test_posix.nim
@@ -0,0 +1,19 @@
+import unittest
+import osproc
+import sequtils
+import strutils
+
+import ../psutil
+
+
+test "test users":
+    let output = execProcess("who")
+    let lines = output.strip().splitlines()
+
+    let users = mapIt( lines, it.splitWhitespace()[0] )
+    check( len(users) == len(psutil.users()) )
+
+    let terminals = mapIt( lines, it.splitWhitespace()[1] )
+    for u in psutil.users():
+        check( u.name in users )
+        check( u.terminal in terminals )
