97e7b8dad47de8dcdad129c72521bbddd096f6a7
zhoupeng <zhoupeng@xjgreat.com>
2019-12-15 22:38:09 +0800

    virtual_memory

--- src/psutil/psutil_macosx.nim
+++ src/psutil/psutil_macosx.nim
@@ -1,4 +1,4 @@
-import posix,segfaults
+import posix, segfaults
 import times except Time
 import common
 import strutils
@@ -6,7 +6,7 @@ include "system/ansi_c"
 # https://reviews.freebsd.org/rS317061
 
 const
-    CTL_KERN = 1.cint      #
+    CTL_KERN = 1.cint          #
     KERN_SUCCESS = 0           # mach/kern_return.h.
     CTL_VM = 2.cint
     KERN_BOOTTIME = 21.cint
@@ -31,9 +31,10 @@ var CPU_STATE_SYSTEM {.importc: "CPU_STATE_SYSTEM",
 var CPU_STATE_IDLE {.importc: "CPU_STATE_IDLE",
         header: "<mach/machine.h>".}: cint
 var CLK_TCK {.importc: "CLK_TCK", header: "<sys/time.h>".}: cdouble
-var CTL_HW  {.importc: "CTL_HW",header: "<sys/sysctl.h>".}: cint
-var HW_NCPU  {.importc: "HW_NCPU",header: "<sys/sysctl.h>".}: cint
-var HW_AVAILCPU  {.importc: "HW_AVAILCPU",header: "<sys/sysctl.h>".}: cint
+var CTL_HW {.importc: "CTL_HW", header: "<sys/sysctl.h>".}: cint
+var HW_NCPU {.importc: "HW_NCPU", header: "<sys/sysctl.h>".}: cint
+var HW_AVAILCPU {.importc: "HW_AVAILCPU", header: "<sys/sysctl.h>".}: cint
+var HW_MEMSIZE {.importc: "HW_MEMSIZE", header: "<sys/sysctl.h>".}: cint
 
 
 type host_cpu_load_info_data_t {.importc: "host_cpu_load_info_data_t",
@@ -56,11 +57,35 @@ type Vmmeter {.importc: "struct vmmeter", header: "<sys/vmmeter.h>",
     v_soft: cint
     v_syscall: cint
 
+type VmStatistics {.importc: "struct vm_statistics",
+        header: "<mach/vm_statistics.h>", pure, incompleteStruct,
+        nodecl.} = object
+    free_count: cint        # of pages free
+    active_count: cint      # of pages active
+    inactive_count: cint    # of pages inactive
+    wire_count: cint        # of pages wired down
+                            #[
+                            * NB: speculative pages are already accounted for in "free_count",
+                            * so "speculative_count" is the number of "free" pages that are
+                            * used to hold data that was read speculatively from disk but
+                            * haven't actually been used by anyone so far.
+                            *]#
+    speculative_count: cint # of pages speculative
+                        # type VmStatistics64 {.importc: "struct vm_statistics64", header: "<mach/vm_statistics64.h>", pure, incompleteStruct,nodecl.} = object
+                            #     free_count:cint # of pages free
+                            #     active_count:cint  # of pages active
+                            #     inactive_count:cint # of pages inactive
+                            #     wire_count:cint  # of pages wired down
+                            #Used by all architectures
+type vm_statistics_t = ptr VmStatistics
+type vm_statistics_data_t = VmStatistics
+
 proc sysctl(x: pointer, y: cint, z: pointer,
             a: var csize_t, b: pointer, c: int): cint {.
             importc: "sysctl", nodecl.}
 
-proc sysctlbyname( name:  cstring; oldp:ptr cint; oldlenp: ptr csize_t;newp: pointer; newlen:csize_t ):cint {.importc: "sysctl", nodecl.}
+proc sysctlbyname(name: cstring; oldp: ptr cint; oldlenp: ptr csize_t;
+        newp: pointer; newlen: csize_t): cint {.importc: "sysctl", nodecl.}
 
 proc mach_host_self(): mach_port_t{.importc: "mach_host_self",
         header: "<mach/mach_init.h>", nodecl.}
@@ -71,7 +96,8 @@ proc mach_task_self(): cint{.importc: "mach_task_self",
 proc mach_error_string(): string{.importc: "mach_error_string",
         header: "<mach/thread_act.h>", nodecl, varargs.}
 proc host_statistics(a: mach_port_t; b: cint; c: host_info_t;
-        d: ptr mach_msg_type_number_t): cint{.importc: "host_statistics", header: "<mach/mach_host.h>", nodecl, varargs.}
+        d: ptr mach_msg_type_number_t): cint{.importc: "host_statistics",
+                header: "<mach/mach_host.h>", nodecl, varargs.}
 
 proc host_statistics64(a: mach_port_t; b: cint; c: host_info_t;
         d: ptr mach_msg_type_number_t): cint{.importc: "host_statistics64",
@@ -80,7 +106,7 @@ proc host_statistics64(a: mach_port_t; b: cint; c: host_info_t;
 proc boot_time*(): int =
     ## Return the system boot time expressed in seconds since the epoch, Integer type.
     var
-        mib: array[0..3,cint]
+        mib: array[0..3, cint]
         boot_time: Time
         len: csize_t
         r: Timeval
@@ -136,17 +162,20 @@ proc cpu_stats*(): tuple[ctx_switches, interrupts, soft_interrupts,
     return (vmstat.v_swtch.int, vmstat.v_intr.int, vmstat.v_soft.int,
             vmstat.v_syscall.int)
 
-type StructProc {.importc: "struct proc", header: "<sys/types.h>", pure, incompleteStruct,nodecl.} = object
+type StructProc {.importc: "struct proc", header: "<sys/types.h>", pure,
+        incompleteStruct, nodecl.} = object
     p_pid: cint
 var KERN_PROC {.importc: "KERN_PROC",
         header: "<sys/types.h>", nodecl.}: cint
 var KERN_PROC_ALL {.importc: "KERN_PROC_ALL",
         header: "<sys/types.h>", nodecl.}: cint
 type StructKinfoProc {.importc: "struct kinfo_proc",
-        header: "<sys/types.h>", pure, incompleteStruct,nodecl.} = object # https://opensource.apple.com/source/xnu/xnu-1456.1.26/bsd/sys/sysctl.h.auto.html
+        header: "<sys/types.h>", pure, incompleteStruct,
+                nodecl.} = object # https://opensource.apple.com/source/xnu/xnu-1456.1.26/bsd/sys/sysctl.h.auto.html
     kp_proc: StructProc
 
-proc get_proc_list(procList: ptr StructKinfoProc; procCount: ptr csize_t):int {.inline,nodecl.} =
+proc get_proc_list(procList: ptr StructKinfoProc; procCount: ptr csize_t): int {.inline, nodecl.} =
+       
     var
         size, size2: csize_t
         err: cint
@@ -166,21 +195,21 @@ proc get_proc_list(procList: ptr StructKinfoProc; procCount: ptr csize_t):int {.
             # PyErr_SetFromOSErrnoWithSyscall("sysctl(KERN_PROC_ALL)")
             return 1
         size2 = size + (size shr 3) # add some
-        if (size2 > size):
-            ptrr =  c_malloc(size2)
+        if size2 > size:
+            ptrr = c_malloc(size2)
             if (ptrr == nil):
-                ptrr =  c_malloc(size)
+                ptrr = c_malloc(size)
             else:
                 size = size2
         else:
-            ptrr =  c_malloc(size)
-        if (ptrr == nil):
+            ptrr = c_malloc(size)
+        if ptrr == nil:
             # PyErr_NoMemory()
             return 1;
-        if (sysctl(mib3.addr, 3, ptrr, size, nil, 0) == -1):
+        if sysctl(mib3.addr, 3, ptrr, size, nil, 0) == -1:
             err = errno
             c_free(ptrr)
-            if (err != ENOMEM):
+            if err != ENOMEM:
                 # PyErr_SetFromOSErrnoWithSyscall("sysctl(KERN_PROC_ALL)")
                 return 1
         else:
@@ -197,7 +226,7 @@ proc get_proc_list(procList: ptr StructKinfoProc; procCount: ptr csize_t):int {.
 
 proc pids*(): seq[int] =
     var
-        proclist:ptr  StructKinfoProc
+        proclist: ptr StructKinfoProc
         orig_address: ptr StructKinfoProc
         num_processes: csize_t
         idx: csize_t
@@ -221,7 +250,7 @@ proc pids*(): seq[int] =
         # if (PyList_Append(py_retlist, py_pid))
             # discard# goto error;
         # Py_CLEAR(py_pid);
-        # proclist.inc 
+        # proclist.inc
         idx.inc
 
     c_free(orig_address)
@@ -231,7 +260,7 @@ proc pids*(): seq[int] =
 proc cpu_count_logical*(): cint =
     ## shared with BSD
     var
-        mib: array[0..3,cint]
+        mib: array[0..3, cint]
         len: csize_t
         r: cint
     mib[0] = CTL_HW
@@ -246,17 +275,66 @@ proc cpu_count_logical*(): cint =
 proc cpu_count_physical*(): int =
     ## just OSX
     var
-        mib: array[0..3,cint]
+        mib: array[0..3, cint]
         len: csize_t
         r: cint
     mib[0] = CTL_HW
     mib[1] = HW_AVAILCPU
     len = sizeof(r).csize_t
-    if sysctl(mib.addr, 2, addr(r), len, nil, 0) == -1:
+    if sysctl(mib.addr, 2, addr(r), len, nil, 0) != 0:
         return -1
     else:
         return r
 
+proc sys_vminfo(vmstat: ptr vm_statistics_data_t): int =
+
+    var
+        count: mach_msg_type_number_t = cast[mach_msg_type_number_t](sizeof(
+        vmstat[]) div sizeof(cint))
+        mport = mach_host_self()
+
+    let ret = host_statistics(mport, HOST_VM_INFO, cast[host_info_t](
+            vmstat), count.addr);
+    if (ret != KERN_SUCCESS):
+        raise newException(OSError, "host_statistics(HOST_VM_INFO) syscall failed: $1" %
+                mach_error_string(ret))
+        # return 0
+    mach_port_deallocate(mach_task_self(), mport);
+    return 1;
+
+
+proc virtual_memory*(): VirtualMemory =
+    ## Return system virtual memory stats.
+    ## See:
+    ## https://opensource.apple.com/source/system_cmds/system_cmds-790/vm_stat.tproj/vm_stat.c.auto.html
+    var
+        mib: array[0..2, cint]
+        len: csize_t
+        total:uint64
+        vm: vm_statistics_data_t
+    let PAGESIZE = sysconf( SC_PAGE_SIZE )
+    mib[0] = CTL_HW
+    mib[1] = HW_MEMSIZE
+    len = sizeof(total).csize_t
+    if sysctl(mib.addr, 2, addr(total), len, nil, 0) == -1:
+        discard
+        # PyErr_SetFromErrno(PyExc_OSError);
+    else:
+        discard
+        # PyErr_Format(
+        #         PyExc_RuntimeError, "sysctl(HW_MEMSIZE) syscall failed");
+        # return nil
+    let ret = sys_vminfo(vm.addr)
+    echo ret
+    if not 1 == ret:
+        return 
+    return VirtualMemory(total: total.int,
+        active: vm.active_count * PAGESIZE,
+        inactive: vm.inactive_count * PAGESIZE,
+        # wire: vm.wire_count * PAGESIZE,
+        free: vm.free_count * PAGESIZE,
+        # speculative: vm.speculative_count * PAGESIZE
+        )
 
 when isMainModule:
     echo boot_time()
@@ -266,3 +344,4 @@ when isMainModule:
     # echo pids()
     echo cpu_count_logical()
     echo cpu_count_physical()
+    echo virtual_memory()
