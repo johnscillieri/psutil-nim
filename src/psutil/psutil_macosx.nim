f66ad22f00992e420be4c24ac261d63c0c2f1977
zhoupeng <zhoupeng@xjgreat.com>
2019-12-16 09:50:53 +0800

    o

--- src/psutil/psutil_macosx.nim
+++ src/psutil/psutil_macosx.nim
@@ -66,14 +66,14 @@ type VmStatistics {.importc: "struct vm_statistics",
     wire_count: cint        # of pages wired down
     pageins: cint
     pageouts: cint
-                            #[
+        #[
                             * NB: speculative pages are already accounted for in "free_count",
                             * so "speculative_count" is the number of "free" pages that are
                             * used to hold data that was read speculatively from disk but
                             * haven't actually been used by anyone so far.
                             *]#
     speculative_count: cint # of pages speculative
-                        # type VmStatistics64 {.importc: "struct vm_statistics64", header: "<mach/vm_statistics64.h>", pure, incompleteStruct,nodecl.} = object
+                            # type VmStatistics64 {.importc: "struct vm_statistics64", header: "<mach/vm_statistics64.h>", pure, incompleteStruct,nodecl.} = object
                             #     free_count:cint # of pages free
                             #     active_count:cint  # of pages active
                             #     inactive_count:cint # of pages inactive
@@ -81,13 +81,64 @@ type VmStatistics {.importc: "struct vm_statistics",
                             #Used by all architectures
 type vm_statistics_t = ptr VmStatistics
 type vm_statistics_data_t = VmStatistics
-type xsw_usage {.importc: "struct xsw_usage", header: "<sys/sysctl.h>",nodecl.} = object
+type xsw_usage {.importc: "struct xsw_usage", header: "<sys/sysctl.h>",
+        nodecl.} = object
     xsu_total: uint64
     xsu_avail: uint64
     xsu_used: uint64
     xsu_pagesize: uint32
     xsu_encrypted: bool
-    
+
+type fsid_t = object
+    val:array[2,cint]
+
+type statfs {.importc: "struct statfs", header: "<sys/syscall.h>",pure, incompleteStruct,nodecl.} = object
+    f_type: clong
+    f_bsize: clong
+    f_blocks: clong
+    f_bfree: clong
+    f_bavail: clong
+    f_files: clong
+    f_ffree: clong
+    f_fsid: fsid_t
+    f_flags:uint64
+    f_namelen: clong
+ 
+const MNT_SYNCHRONOUS = 0x00000002 # file system written synchronously
+const MNT_RDONLY      = 0x00000001 # read only filesystem
+const MNT_NOEXEC      = 0x00000004 # can't exec from filesystem
+const MNT_NOSUID      = 0x00000008 # don't honor setuid bits on fs
+const MNT_NODEV       = 0x00000010 # don't interpret special files
+const MNT_UNION       = 0x00000020 # union with underlying filesystem
+const MNT_ASYNC       = 0x00000040 # file system written asynchronously
+const MNT_CPROTECT    = 0x00000080 # file system supports content protection
+const MNT_EXPORTED    = 0x00000100 # file system is exported
+const MNT_QUARANTINE  = 0x00000400 # file system is quarantined
+const MNT_LOCAL       = 0x00001000 # filesystem is stored locally
+const MNT_QUOTA       = 0x00002000 # quotas are enabled on filesystem
+const MNT_ROOTFS      = 0x00004000 # identifies the root filesystem
+const MNT_DOVOLFS     = 0x00008000 # FS supports volfs (deprecated)
+const MNT_DONTBROWSE  = 0x00100000 # FS is not appropriate path to user data
+const MNT_IGNORE_OWNERSHIP = 0x00200000 # VFS will ignore ownership info on FS objects
+const MNT_AUTOMOUNTED = 0x00400000 # filesystem was mounted by automounter
+const MNT_JOURNALED   = 0x00800000 # filesystem is journaled
+const MNT_NOUSERXATTR = 0x01000000 # Don't allow user extended attributes
+const MNT_DEFWRITE    = 0x02000000 # filesystem should defer writes
+const MNT_MULTILABEL  = 0x04000000 # MAC support for individual labels
+const MNT_NOATIME     = 0x10000000 # disable update of file access time
+const MNT_UPDATE      = 0x00010000
+const MNT_RELOAD      = 0x00040000
+const MNT_FORCE       = 0x00080000
+
+const MNT_CMDFLAGS flags{ MNT_UPDATE MNT_RELOAD MNT_FORCE }
+
+const MNT_WAIT = 1
+const MNT_NOWAIT = 2
+
+proc strlcat(dst:ptr UncheckedArray[char];src: cstring ,size:csize_t ) : csize_t {.importc: "strlcat", nodecl.}
+proc getfsstat(buf:ptr UncheckedArray[statfs]; bufsize: clong;mode: cint) : cint {.importc: "getfsstat", nodecl.}
+    #https://www.freebsd.org/cgi/man.cgi?query=getfsstat&sektion=2&manpath=freebsd-release-ports
+
 proc sysctl(x: pointer, y: cint, z: pointer,
             a: var csize_t, b: pointer, c: int): cint {.
             importc: "sysctl", nodecl.}
@@ -182,8 +233,9 @@ type StructKinfoProc {.importc: "struct kinfo_proc",
                 nodecl.} = object # https://opensource.apple.com/source/xnu/xnu-1456.1.26/bsd/sys/sysctl.h.auto.html
     kp_proc: StructProc
 
-proc get_proc_list(procList: ptr StructKinfoProc; procCount: ptr csize_t): int {.inline, nodecl.} =
-       
+proc get_proc_list(procList: ptr StructKinfoProc;
+        procCount: ptr csize_t): int {.inline, nodecl.} =
+
     var
         size, size2: csize_t
         err: cint
@@ -318,9 +370,9 @@ proc virtual_memory*(): VirtualMemory =
     var
         mib: array[0..2, cint]
         len: csize_t
-        total:uint64
+        total: uint64
         vm: vm_statistics_data_t
-    let PAGESIZE = sysconf( SC_PAGE_SIZE )
+    let PAGESIZE = sysconf(SC_PAGE_SIZE)
     mib[0] = CTL_HW
     mib[1] = HW_MEMSIZE
     len = sizeof(total).csize_t
@@ -335,7 +387,7 @@ proc virtual_memory*(): VirtualMemory =
     let ret = sys_vminfo(vm.addr)
     echo ret
     if not 1 == ret:
-        return 
+        return
     return VirtualMemory(total: total.int,
         active: vm.active_count * PAGESIZE,
         inactive: vm.inactive_count * PAGESIZE,
@@ -343,14 +395,14 @@ proc virtual_memory*(): VirtualMemory =
         free: vm.free_count * PAGESIZE,
         # speculative: vm.speculative_count * PAGESIZE
         )
-    
+
 proc swap_memory*(): SwapMemory =
     var
         mib: array[0..2, cint]
         len: csize_t
-        totals:xsw_usage
+        totals: xsw_usage
         vm: vm_statistics_data_t
-    let PAGESIZE = sysconf( SC_PAGE_SIZE )
+    let PAGESIZE = sysconf(SC_PAGE_SIZE)
     mib[0] = CTL_HW
     mib[1] = HW_MEMSIZE
     len = sizeof(totals).csize_t
@@ -364,10 +416,123 @@ proc swap_memory*(): SwapMemory =
         # return nil
     let ret = sys_vminfo(vm.addr)
     if not 1 == ret:
-        return 
-    let percent = usage_percent( totals.xsu_used, totals.xsu_total, 1 )
-    return SwapMemory( total:totals.xsu_total.int, used:totals.xsu_used.int, free:totals.xsu_avail.int,
-        percent:percent, sin:vm.pageins * PAGESIZE, sout:vm.pageouts * PAGESIZE )   
+        return
+    let percent = usage_percent(totals.xsu_used, totals.xsu_total, 1)
+    return SwapMemory(total: totals.xsu_total.int, used: totals.xsu_used.int,
+        free: totals.xsu_avail.int,
+        percent: percent, sin: vm.pageins * PAGESIZE, sout: vm.pageouts * PAGESIZE)
+
+proc disk_partitions*(all = false): seq[DiskPartition] =
+    var
+        num:cint
+        i:int
+        len: csize_t
+        flags: uint64
+        opts: array[400, char]
+        fs:ptr UncheckedArray[statfs]
+    # get the number of mount points
+    # Py_BEGIN_ALLOW_THREADS
+    num = getfsstat(nil, 0, MNT_NOWAIT)
+    # Py_END_ALLOW_THREADS  
+    # if (num == -1) :
+    #     PyErr_SetFromErrno(PyExc_OSError);
+    #     goto error;
+    
+    echo num
+    len = csize_t( sizeof(fs[]).uint * num.uint )
+    let ptrr = c_malloc(len)
+    if (ptrr == nil) :
+        discard
+        #     PyErr_SetFromErrno(PyExc_OSError);
+        #     goto error;
+    # Py_BEGIN_ALLOW_THREADS
+    num = getfsstat(fs, len.clong, MNT_NOWAIT)
+    # Py_END_ALLOW_THREADS    
+    if (num == -1) :
+        discard
+        # PyErr_SetFromErrno(PyExc_OSError);
+        # goto error
+    var f:ptr UncheckedArray[statfs] 
+    while i < num :
+        opts[0] = '0'
+        f  = cast[ptr UnCheckedArray[statfs]](fs)
+        flags = f[i].f_flags
+        # see sys/mount.h
+        if (flags and MNT_RDONLY) != 0:
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), "ro", sizeof(opts).csize_t)
+        else:
+            # strlcat(opts, "rw", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), "rw", sizeof(opts).csize_t)
+        if (flags and MNT_SYNCHRONOUS) != 0:
+            # strlcat(opts, ",sync", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",sync", sizeof(opts).csize_t)
+        if (flags and MNT_NOEXEC) != 0:
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), "noexec", sizeof(opts).csize_t)
+            # strlcat(opts, ",noexec", sizeof(opts));
+        if (flags and MNT_NOSUID) != 0:
+            # strlcat(opts, ",nosuid", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",nosuid", sizeof(opts).csize_t)
+        if (flags and MNT_UNION) != 0:
+            # strlcat(opts, ",union", sizeof(opts))
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",union", sizeof(opts).csize_t)
+        if (flags and MNT_ASYNC) != 0:
+            # strlcat(opts, ",async", sizeof(
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",async", sizeof(opts).csize_t)
+        if (flags and MNT_EXPORTED) != 0:
+            # strlcat(opts, ",exported", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",exported", sizeof(opts).csize_t)
+        if (flags and MNT_QUARANTINE) != 0:
+            # strlcat(opts, ",quarantine", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",quarantine", sizeof(opts).csize_t)
+        if (flags and MNT_LOCAL) != 0:
+            # strlcat(opts, ",local", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",local", sizeof(opts).csize_t)
+        if (flags and MNT_QUOTA) != 0:
+            # strlcat(opts, ",quota", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",quota", sizeof(opts).csize_t)
+        if (flags and MNT_ROOTFS) != 0:
+            # strlcat(opts, ",rootfs", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",rootfs", sizeof(opts).csize_t)
+        if (flags and MNT_DOVOLFS) != 0:
+            # strlcat(opts, ",dovolfs", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",dovolfs", sizeof(opts).csize_t)
+        if (flags and MNT_DONTBROWSE) != 0:
+            # strlcat(opts, ",dontbrowse", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",dontbrowse", sizeof(opts).csize_t)
+        if (flags and MNT_IGNORE_OWNERSHIP) != 0:
+            # strlcat(opts, ",ignore-ownership", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",ignore-ownership", sizeof(opts).csize_t)
+        if (flags and MNT_AUTOMOUNTED) != 0:
+            # strlcat(opts, ",automounted", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",automounted", sizeof(opts).csize_t)
+        if (flags and MNT_JOURNALED) != 0:
+            # strlcat(opts, ",journaled", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",journaled", sizeof(opts).csize_t)
+        if (flags and MNT_NOUSERXATTR) != 0:
+            # strlcat(opts, ",nouserxattr", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",nouserxattr", sizeof(opts).csize_t)
+        if (flags and MNT_DEFWRITE) != 0:
+            # strlcat(opts, ",defwrite", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",defwrite", sizeof(opts).csize_t)
+        if (flags and MNT_MULTILABEL) != 0:
+            # strlcat(opts, ",multilabel", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",multilabel", sizeof(opts).csize_t)
+        if (flags and MNT_NOATIME) != 0:
+            # strlcat(opts, ",noatime", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",noatime", sizeof(opts).csize_t)
+        if (flags and MNT_UPDATE) != 0:
+            # strlcat(opts, ",update", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",update", sizeof(opts).csize_t)
+        if (flags and MNT_RELOAD) != 0:
+            # strlcat(opts, ",reload", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",reload", sizeof(opts).csize_t)
+        if (flags and MNT_FORCE) != 0:
+            # strlcat(opts, ",force", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",force", sizeof(opts).csize_t)
+        if (flags and MNT_CMDFLAGS) != 0:
+            # strlcat(opts, ",cmdflags", sizeof(opts));
+            discard strlcat(cast[ptr UncheckedArray[char]](opts.addr), ",cmdflags", sizeof(opts).csize_t)
+        i.inc
 
 when isMainModule:
     echo boot_time()
@@ -379,3 +544,4 @@ when isMainModule:
     echo cpu_count_physical()
     echo virtual_memory()
     echo swap_memory()
+    echo disk_partitions()
