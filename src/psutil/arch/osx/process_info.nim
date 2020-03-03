862ad2dfb7645248df3456fd511c88a400e2f8aa
Juan Carlos <JuanCarlospaco@gmail.com>
2020-03-03 16:16:36 -0300

    Add Mac Support, see https://github.com/johnscillieri/psutil-nim/pull/5

--- src/psutil/arch/osx/process_info.nim
+++ src/psutil/arch/osx/process_info.nim
@@ -1,50 +1,61 @@
 import ./types
 import ./socket
 
-const MAXPATHLEN      = 1024
+const 
+  MAXPATHLEN = 1024
+  PROC_PIDLISTFDS* = 1.cint
+  PROC_PIDLISTFD_SIZE* = 8.cint
 
-const PROC_PIDLISTFDS* = 1.cint
-const PROC_PIDLISTFD_SIZE* = 8.cint
 # defns of process file desc type  https://opensource.apple.com/source/xnu/xnu-1456.1.26/bsd/sys/proc_info.h.auto.html
-const PROX_FDTYPE_ATALK* = 0.cint
-const PROX_FDTYPE_VNODE* = 1.cint
-const PROX_FDTYPE_SOCKET* = 2.cint
-const PROX_FDTYPE_PSHM* = 3.cint
-const PROX_FDTYPE_PSEM* = 4.cint
-const PROX_FDTYPE_KQUEUE* = 5.cint
-const PROX_FDTYPE_PIPE* = 6.cint
-const PROX_FDTYPE_FSEVENTS* = 7.cint
-const PROC_PIDFDVNODEPATHINFO* = 2.cint
+const 
+  PROX_FDTYPE_ATALK* = 0.cint
+  PROX_FDTYPE_VNODE* = 1.cint
+  PROX_FDTYPE_SOCKET* = 2.cint
+  PROX_FDTYPE_PSHM* = 3.cint
+  PROX_FDTYPE_PSEM* = 4.cint
+  PROX_FDTYPE_KQUEUE* = 5.cint
+  PROX_FDTYPE_PIPE* = 6.cint
+  PROX_FDTYPE_FSEVENTS* = 7.cint
+  PROC_PIDFDVNODEPATHINFO* = 2.cint
 
-type proc_fdinfo* {.importc:"struct proc_fdinfo",header:"<sys/proc_info.h>".} = object
-    proc_fdtype*:cint
-    proc_fd*:cint
 
-type vnode_fdinfowithpath* {.importc:"struct vnode_fdinfowithpath",header:"<sys/proc_info.h>".} = object
+type proc_fdinfo* {.importc: "struct proc_fdinfo", header: "<sys/proc_info.h>".} = object
+    proc_fdtype*: cint
+    proc_fd*: cint
+
+
+type vnode_fdinfowithpath* {.importc: "struct vnode_fdinfowithpath", header: "<sys/proc_info.h>".} = object
 
 
 type proc_fileinfo* = object
-    fi_openflags*:uint32
-    fi_status*:uint32
-    fi_offset*:off_t
-    vip_path*:array[MAXPATHLEN,char]
+    fi_openflags*: uint32
+    fi_status*: uint32
+    fi_offset*: off_t
+    vip_path*: array[MAXPATHLEN, char]
 
-type socket_fdinfo* {.importc:"struct socket_fdinfo",header:"<sys/proc_info.h>".} = object
-    pfi*:proc_fileinfo
-    psi*:socket_info
+
+type socket_fdinfo* {.importc: "struct socket_fdinfo", header: "<sys/proc_info.h>".} = object
+    pfi*: proc_fileinfo
+    psi*: socket_info
+
 
 # https://opensource.apple.com/source/xnu/xnu-2422.1.72/libsyscall/wrappers/libproc/libproc.h.auto.html
-proc proc_pidinfo*(pid:cint,flavor:cint,arg:uint64,buffer:pointer,buffer_size:cint):cint{.importc:"proc_pidinfo",header:"<libproc.h>".}
+proc proc_pidinfo*(pid: cint, flavor: cint, arg: uint64, buffer: pointer, buffer_size: cint): cint {.importc: "proc_pidinfo", header: "<libproc.h>".}
 
-proc proc_pidfdinfo*(pid:cint,flavor:cint,fd:cint,buffer:pointer,buffer_size:cint):cint{.importc:"proc_pidfdinfo",header:"<libproc.h>".}
+
+proc proc_pidfdinfo*(pid: cint, flavor: cint, fd: cint, buffer: pointer, buffer_size: cint): cint {.importc: "proc_pidfdinfo", header: "<libproc.h>".}
+
 
 when isMainModule:
     var x {.importc: "PROC_PIDLISTFDS", header: "<sys/proc_info.h>".}: cint
     assert x == PROC_PIDLISTFDS
+
     var y {.importc: "PROX_FDTYPE_ATALK", header: "<sys/proc_info.h>".}: cint
     assert y == PROX_FDTYPE_ATALK
+
     var z {.importc: "PROX_FDTYPE_VNODE", header: "<sys/proc_info.h>".}: cint
     assert z == PROX_FDTYPE_VNODE
+
     var z1 {.importc: "PROX_FDTYPE_SOCKET", header: "<sys/proc_info.h>".}: cint
     assert z1 == PROX_FDTYPE_SOCKET
 
