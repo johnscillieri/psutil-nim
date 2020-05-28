438d76002602fda951852d188a25c9212e1f5e4f
Kaushal Modi <kaushal.modi@gmail.com>
2020-05-28 11:23:28 -0400

    Fix test for the case where `who` returns nothing

--- tests/test_posix.nim
+++ tests/test_posix.nim
@@ -7,13 +7,14 @@ import ../src/psutil
 
 
 test "test users":
-    let output = execProcess("who")
-    let lines = output.strip().splitlines()
+    let output = execProcess("who").strip()
+    if output != "":
+        let lines = output.splitlines()
+        echo "dbg: ", lines[0].splitWhitespace()
+        let users = mapIt( lines, it.splitWhitespace()[0] )
+        check( len(users) == len(psutil.users()) )
 
-    let users = mapIt( lines, it.splitWhitespace()[0] )
-    check( len(users) == len(psutil.users()) )
-
-    let terminals = mapIt( lines, it.splitWhitespace()[1] )
-    for u in psutil.users():
-        check( u.name in users )
-        check( u.terminal in terminals )
+        let terminals = mapIt( lines, it.splitWhitespace()[1] )
+        for u in psutil.users():
+            check( u.name in users )
+            check( u.terminal in terminals )
